fbcdd019d633e97823b5f445304aa75c6ede3de8
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-21 01:10:52 -0500

    adding functions

--- CollectionViewColors/ViewController.swift
+++ CollectionViewColors/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpCollectionView(collection: CollectionViewExample)
     }
 
-
+    //variables
+    
+    //IBOutlet
+    @IBOutlet var CollectionViewExample: UICollectionView!
+    
+    //IBActions
+    
+    //Functions
+    
+    func setUpCollectionView(collection:UICollectionView) {
+        collection.delegate = self
+        collection.dataSource = self
+    }
+    
+    //System Functions
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+        
+        return cell
+    }
 }
 
