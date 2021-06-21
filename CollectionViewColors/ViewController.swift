018f8f1911d6a32dc664a89baea09da7065d2de1
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-21 01:24:14 -0500

    adding colors collectionview

--- CollectionViewColors/ViewController.swift
+++ CollectionViewColors/ViewController.swift
@@ -30,13 +30,23 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     //System Functions
+    
+    //number of items
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 50
+        return 500
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellColor", for: indexPath)
+        
+        //addign colors
+        
+        let red = CGFloat(Int(arc4random_uniform(155))+100)
+        let blue = CGFloat(Int(arc4random_uniform(155))+100)
+        let green = CGFloat(Int(arc4random_uniform(155))+100)
+        
+        cell.backgroundColor = UIColor(displayP3Red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
         
         return cell
     }
